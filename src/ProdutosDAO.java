530b3ec9378c1dff630eca2549fc5f7bef9b810c
dudamorales <mariaeduardamorales08@outlook.com>
2026-04-06 21:22:49 -0300

    Listagem de produtos funcionando

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 
 /**
  *
@@ -22,21 +19,56 @@ public class ProdutosDAO {
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
+    // SALVAR PRODUTO NO BANCO 
     public void cadastrarProduto (ProdutosDTO produto){
+    String sql = "INSERT INTO produtos (nome, valor, status) VALUES (?, ?, ?)";
+    
+    conn = new conectaDAO().connectDB();
+    
+    try {
+        prep = conn.prepareStatement(sql);
+        prep.setString(1, produto.getNome());
+        prep.setInt(2, produto.getValor());
+        prep.setString(3, produto.getStatus());
         
+        prep.executeUpdate();
         
-        //conn = new conectaDAO().connectDB();
+        JOptionPane.showMessageDialog(null, "Produto cadastrado com sucesso!");
         
-        
+    } catch (Exception e) {
+    JOptionPane.showMessageDialog(null, "Erro ao cadastrar produto: " + e.getMessage());
+}     
     }
     
+    // listagem funcionando
     public ArrayList<ProdutosDTO> listarProdutos(){
+    
+    String sql = "SELECT * FROM produtos";
+    
+    conn = new conectaDAO().connectDB();
+    
+    try {
+        prep = conn.prepareStatement(sql);
+        resultset = prep.executeQuery();
         
-        return listagem;
+        listagem.clear();
+        
+        while(resultset.next()){
+            ProdutosDTO produto = new ProdutosDTO();
+            
+            produto.setId(resultset.getInt("id"));
+            produto.setNome(resultset.getString("nome"));
+            produto.setValor(resultset.getInt("valor"));
+            produto.setStatus(resultset.getString("status"));
+            
+            listagem.add(produto);
+        }
+        
+    } catch (Exception e) {
+        JOptionPane.showMessageDialog(null, "Erro ao listar produtos");
     }
     
-    
-    
-        
+    return listagem;
+}    
 }
 

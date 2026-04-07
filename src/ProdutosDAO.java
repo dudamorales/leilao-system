28d8d97fc4d13b2d3d26453e2e3d517858871d26
dudamorales <mariaeduardamorales08@outlook.com>
2026-04-06 22:18:13 -0300

    Listagem de produtos vendidos

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -86,6 +86,33 @@ public class ProdutosDAO {
     } catch (Exception e) {
         JOptionPane.showMessageDialog(null, "Erro ao vender produto");
     }
-}
-}
+} 
+    public ArrayList<ProdutosDTO> listarProdutosVendidos(){
 
+    String sql = "SELECT * FROM produtos WHERE status = 'Vendido'";
+    
+    conn = new conectaDAO().connectDB();
+    listagem.clear();
+
+    try {
+        prep = conn.prepareStatement(sql);
+        resultset = prep.executeQuery();
+
+        while(resultset.next()){
+            ProdutosDTO produto = new ProdutosDTO();
+
+            produto.setId(resultset.getInt("id"));
+            produto.setNome(resultset.getString("nome"));
+            produto.setValor(resultset.getInt("valor"));
+            produto.setStatus(resultset.getString("status"));
+
+            listagem.add(produto);
+        }
+
+    } catch (Exception e) {
+        JOptionPane.showMessageDialog(null, "Erro ao listar produtos vendidos");
+    }
+
+    return listagem;
+}
+}
